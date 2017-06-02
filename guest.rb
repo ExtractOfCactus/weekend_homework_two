4fdd9764c940fea0d332f5ddc6cdcb96ca7cf6b2
Glen Young <extractofcactus@icloud.com>
2017-06-02 16:26:19 +0100

    can drunk sing

--- guest.rb
+++ guest.rb
@@ -2,16 +2,26 @@ class Guest
   attr_reader :name, :fav_song
   attr_accessor :wallet
 
-  def initialize(name, wallet, fav_song)
+  def initialize(name, wallet, fav_song, drunk_level)
     @name = name
     @wallet = wallet
     @fav_song = fav_song
+    @drunk_level = drunk_level
   end
 
   def sing(song)
     return "#{song.lyrics().upcase()} AWWWW YEAH!" if song.title() == fav_song.title()
     return song.lyrics()
   end
+
+  def sing_drunk(song)
+    if @drunk_level >= 4
+      word_arr = song.lyrics().split (' ')
+      word_arr.shuffle!()
+      return"#{word_arr.join(" ")}"
+    end
+  end
+
 
 
 
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -12,9 +12,9 @@ class TestGuest < MiniTest::Test
     @song_1 = Song.new("Back in Black", "AC/DC", "BaaAAaaAAack, BaaAAaaAAack!")
     @song_2 = Song.new("Sympathy for the Devil", "The Rolling Stones", "Pleased to meet you, hope you guessed my name")
     @song_3 = Song.new("Roxanne", "Sting and The Police", "Put on a red light!")
-    @guest_1 = Guest.new("Rick", 100, @song_1)
-    @guest_2 = Guest.new("Morty", 50, @song_2)
-    @guest_3 = Guest.new("Summer", 50, @song_3)
+    @guest_1 = Guest.new("Rick", 100, @song_1, 5)
+    @guest_2 = Guest.new("Morty", 50, @song_2, 0)
+    @guest_3 = Guest.new("Summer", 50, @song_3, 0)
 
   end
 
@@ -42,6 +42,10 @@ class TestGuest < MiniTest::Test
   #   result = @guest_1.buy_drink(@bar_1)
   #   assert_equal(55, @bar_1.money())
   #   assert_equal(95, @guest.wallet())
+  # end
+
+  # def test_drunk
+  #   assert_equal(nil, @guest_1.sing_drunk(@song_2))
   # end
 
 end
